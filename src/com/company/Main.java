166aa8abd82e6f11f854bb42140b87dc8318af72
climberwz <346875874@qq.com>
2019-06-18 17:44:18 +0800

    hello 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello2");
     }
 }
